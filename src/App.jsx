f3dbcc75df0897057a8899f8ec72da8ca1e1a717
jayanth <jayanths8899@gmail.com>
2024-01-19 20:10:59 +0530

    ready for prod

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,21 @@ const { Header, Content } = Layout;
 
 const App = () => {
   return (
-    <Layout>
-      <Row gutter={[0, 32]}>
-        <Col span={24}>
-          <Header>
-            <AppHeader />
-          </Header>
-        </Col>
-        <Col span={22} offset={1}>
-          <Content>
+    // <Layout>
+    //   <Row gutter={[0, 32]}>
+    //     <Col span={24}>
+    //       <Header>
+    //         <AppHeader />
+    //       </Header>
+    //     </Col>
+    //     <Col span={22} offset={1}>
+    //       <Content>
             <AppRoutes />
-          </Content>
-        </Col>
-      </Row>
-    </Layout>
+    //       </Content>
+    //     </Col>
+    //   </Row>
+    // </Layout>
+    
   );
 };
 
--- src/components/SocialCards/SocialCards.jsx
+++ src/components/SocialCards/SocialCards.jsx
@@ -1,24 +1,26 @@
 import {
-  Button,
-  Card,
-  Col,
-  Image,
+  // Button,
+  // Card,
+  // Col,
+  // Image,
   message,
-  Row,
-  Space,
+  // Row,
+  // Space,
   Spin,
-  Typography,
+  // Typography,
 } from "antd";
 import React, { useEffect, useState } from "react";
-import {
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillGithub,
-} from "react-icons/ai";
+// import {
+//   AiFillTwitterCircle,
+//   AiFillLinkedin,
+//   AiFillGithub,
+// } from "react-icons/ai";
 
-import { CgWebsite } from "react-icons/cg";
-import { SiGmail } from "react-icons/si";
+// import { CgWebsite } from "react-icons/cg";
+// import { SiGmail } from "react-icons/si";
+import './MyComponent.css';
 import { API, AVATAR_API } from "../../constant";
+import { Link } from 'react-router-dom';
 
 const SocialCards = () => {
   const [profiles, setProfiles] = useState([]);
@@ -47,82 +49,20 @@ const SocialCards = () => {
   }
 
   return (
-    <Row gutter={[32, 32]}>
-      {profiles.map((profile, index) => (
-        <Col md={8} lg={8} sm={24} xs={24} key={`${profile.id}_${index}`}>
-          <Card className="social_card">
-            <Space
-              className="social_card_space"
-              direction="vertical"
-              align="center"
-            >
-              <Image
-                className="social_image"
-                preview={false}
-                src={
-                  profile.avatar_url ??
-                  `${AVATAR_API}?name=${profile.username}&background=1890ff&color=fff`
-                }
-              />
-              <Typography.Title level={5}>{profile.username}</Typography.Title>
-              <Typography.Paragraph>{profile.about}</Typography.Paragraph>
-              <Space size={16} wrap>
-                {profile.twitter_username && (
-                  <Button
-                    className="social_button twitter"
-                    href={`https://twitter.com/${profile.twitter_username}`}
-                    type="link"
-                    target="_blank"
-                  >
-                    <AiFillTwitterCircle size={24} />
-                  </Button>
-                )}
-                {profile.linkedin_username && (
-                  <Button
-                    className="social_button linkedin"
-                    href={`https://www.linkedin.com/in/${profile.linkedin_username}`}
-                    type="link"
-                    target="_blank"
-                  >
-                    <AiFillLinkedin size={24} />
-                  </Button>
-                )}
-                {profile.github_username && (
-                  <Button
-                    className="social_button github"
-                    href={`https://github.com/${profile.github_username}`}
-                    type="link"
-                    target="_blank"
-                  >
-                    <AiFillGithub size={24} />
-                  </Button>
-                )}
-                {profile.website_url && (
-                  <Button
-                    className="social_button website"
-                    href={profile.website_url}
-                    type="link"
-                    target="_blank"
-                  >
-                    <CgWebsite size={24} />
-                  </Button>
-                )}
-                {profile.email && (
-                  <Button
-                    className="social_button gmail"
-                    href={`mailto:${profile.email}`}
-                    type="link"
-                    target="_blank"
-                  >
-                    <SiGmail size={24} />
-                  </Button>
-                )}
-              </Space>
-            </Space>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+
+    <nav className="navbar">
+    <div className="navbar-brand">SOFTECH</div>
+    <ul className="navbar-nav">
+      <li className="nav-item">Home</li>
+      <li className="nav-item">About</li>
+      <li className="nav-item">Services</li>
+      <li className="nav-item">Contact</li>
+      <li className="nav-item">
+          <Link to="/signin">Logout</Link>
+        </li>
+
+    </ul>
+  </nav>
   );
 };
 
